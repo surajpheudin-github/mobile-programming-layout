62d9b198a080ff40bac55f5f50dccc3cca061783
Suraj Pheudin <surajpheudin.github@gmail.com>
2022-01-27 20:00:03 +0545

    Feature: Add xml(design) for linear layout

--- app/src/main/java/com/surajphueudin/layout_suraj_pheudin/LinearLayoutActivity.java
+++ app/src/main/java/com/surajphueudin/layout_suraj_pheudin/LinearLayoutActivity.java
@@ -12,13 +12,5 @@ public class LinearLayoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linear_layout);
-
-        // Get the Intent that started this activity and extract the string
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-
-        // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(message);
     }
 }
