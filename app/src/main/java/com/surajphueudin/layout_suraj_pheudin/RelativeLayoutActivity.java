dfd67639135af35eab91cb56b61c272932dda36c
Suraj Pheudin <surajpheudin.github@gmail.com>
2022-01-31 08:33:50 +0545

    Feature: Add svg icon and button on relative layout

--- app/src/main/java/com/surajphueudin/layout_suraj_pheudin/RelativeLayoutActivity.java
+++ app/src/main/java/com/surajphueudin/layout_suraj_pheudin/RelativeLayoutActivity.java
@@ -2,7 +2,9 @@ package com.surajphueudin.layout_suraj_pheudin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RelativeLayoutActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class RelativeLayoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relative_layout);
+    }
+
+    public void backToMainPage(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
