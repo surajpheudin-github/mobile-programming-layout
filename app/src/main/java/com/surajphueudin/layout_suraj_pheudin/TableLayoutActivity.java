1bd1938643e3cf9f2b5cdc542425c38a50ab7362
Suraj Pheudin <surajpheudin.github@gmail.com>
2022-01-31 13:51:12 +0545

    Feature: Add initial onClick event listenser on table layout

--- app/src/main/java/com/surajphueudin/layout_suraj_pheudin/TableLayoutActivity.java
+++ app/src/main/java/com/surajphueudin/layout_suraj_pheudin/TableLayoutActivity.java
@@ -3,6 +3,9 @@ package com.surajphueudin.layout_suraj_pheudin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class TableLayoutActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class TableLayoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_table_layout);
+    }
+
+    public void setClickedButton(View view) {
+        TextView result = (TextView) findViewById(R.id.textView14);
+        Button btn = (Button) findViewById(view.getId());
+        String value = (String) btn.getText();
+        result.setText(value);
     }
 }
